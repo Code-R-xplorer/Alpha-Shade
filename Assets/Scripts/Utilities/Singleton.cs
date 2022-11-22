76763ee0094965336ca914eda1d8cb62527d4fd0
Harry Vallance <harry@robvallance.com>
2022-11-22 16:34:19 +0000

    Optimized singleton script

--- Assets/Scripts/Utilities/Singleton.cs
+++ Assets/Scripts/Utilities/Singleton.cs
@@ -14,20 +14,22 @@ namespace Utilities
                 if (_instance == null)
                 {
                     var objs = FindObjectsOfType(typeof(T)) as T[];
-                    if (objs.Length > 0)
+                    if (objs is { Length: > 0 })
                     {
                         _instance = objs[0];
                     }
 
-                    if (objs.Length > 1)
+                    if (objs is { Length: > 1 })
                     {
                         Debug.LogError("There is more than one " + typeof(T).Name + " in the current scene.");
                     }
 
                     if (_instance == null)
                     {
-                        GameObject obj = new GameObject();
-                        obj.hideFlags = HideFlags.HideAndDontSave;
+                        var obj = new GameObject
+                        {
+                            hideFlags = HideFlags.HideAndDontSave
+                        };
                         _instance = obj.AddComponent<T>();
                     }
                 }
