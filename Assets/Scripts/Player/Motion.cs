c2d5c0cf763b96ed145fd7457f44dd56a25cdf34
TheAmazingHazza <59018823+TheAmazingHazza@users.noreply.github.com>
2023-02-19 21:42:11 +0000

    Implemented sprinting and jumping

--- Assets/Scripts/Player/Motion.cs
+++ Assets/Scripts/Player/Motion.cs
@@ -5,15 +5,33 @@ namespace Player
 {
     public class Motion : MonoBehaviour
     {
-        [SerializeField] private float speed;
+        [SerializeField] private float walkSpeed;
+        [SerializeField] private float sprintSpeed;
+
+        [SerializeField] private float playerHeight = 2;
+        [SerializeField] private float groundDistance = 0.4f;
+        [SerializeField] private LayerMask groundMask;
+        [SerializeField] private float jumpForce = 10f;
+        private float speed;
         private InputManager _input;
 
         private Rigidbody _rb;
+
+        private const float SpeedMultiplier = 100f;
+
+        private bool _isSprinting;
+
+        private bool _isGrounded;
         // Start is called before the first frame update
         void Start()
         {
             _input = InputManager.Instance;
             _rb = GetComponent<Rigidbody>();
+            _input.OnSprint += Sprint;
+            _input.OnStartJump += Jump;
+            sprintSpeed *= SpeedMultiplier;
+            walkSpeed *= SpeedMultiplier;
+            jumpForce *= SpeedMultiplier;
         }
 
         // Update is called once per frame
@@ -21,12 +39,43 @@ namespace Player
         {
             float hMove = _input.MovementInput.x;
             float vMove = _input.MovementInput.y;
+            CheckGrounded();
 
             Vector3 direction = new Vector3(hMove, 0, vMove);
             direction.Normalize();
 
-            _rb.velocity = transform.TransformDirection(direction) * (speed * Time.deltaTime);
+            if (_isSprinting)
+            {
+                speed = sprintSpeed;
+            }
+            else
+            {
+                speed = walkSpeed;
+            }
+            
+            Vector3 targetVelocity = transform.TransformDirection(direction) * (speed * Time.deltaTime);
+            targetVelocity.y = _rb.velocity.y;
+            _rb.velocity = targetVelocity;
 
+        }
+        
+        private void CheckGrounded()
+        {
+            _isGrounded = Physics.CheckSphere(transform.position - new Vector3(0, playerHeight / 2, 0), groundDistance,
+                groundMask);
+        }
+
+        private void Sprint(bool canceled)
+        {
+            _isSprinting = !canceled;
+        }
+        
+        private void Jump()
+        {
+            if (_isGrounded)
+            {
+                _rb.AddForce(transform.up * jumpForce);
+            }
         }
     }
 }
