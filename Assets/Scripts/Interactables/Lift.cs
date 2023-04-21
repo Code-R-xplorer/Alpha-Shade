379025c0ca44e8b1740d0d54a72eed535fc92da3
TheAmazingHazza <59018823+TheAmazingHazza@users.noreply.github.com>
2023-04-21 21:09:53 +0100

    Tech Company Changes
    - Added second and third floors to level
    - Added Lift and lift logic to get between the three floors of the level
    - Added interaction script to interact with the buttons in the lift

---
+++ Assets/Scripts/Interactables/Lift.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using UnityEngine;
+
+namespace Interactables
+{
+    public class Lift : MonoBehaviour
+    {
+        private Animator _animator;
+        private static readonly int Open = Animator.StringToHash("Lift_Open");
+        private static readonly int Close = Animator.StringToHash("Lift_Close");
+
+        [SerializeField] private Transform groundFloor;
+        [SerializeField] private Transform firstFloor;
+        [SerializeField] private Transform secondFloor;
+
+        private GameObject _player;
+
+        private void Start()
+        {
+            _player = GameObject.FindGameObjectWithTag("Player");
+            _animator = GetComponent<Animator>();
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                _animator.Play(Open, -1, 0);
+            }
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                _animator.Play(Close, -1, 0);
+            }
+        }
+
+        public void GoToFloor(int floor)
+        {
+            Transform newTransform = groundFloor;
+            if (floor == 0) newTransform = groundFloor;
+            if (floor == 1) newTransform = firstFloor;
+            if (floor == 2) newTransform = secondFloor;
+            
+            _player.transform.position = newTransform.position;
+        }
+    }
+}
---
+++ Assets/Scripts/Player/Interact.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using Utilities;
+
+namespace Player
+{
+    public class Interact : MonoBehaviour
+    {
+        private LayerMask _layerMask;
+        private void Start()
+        {
+            InputManager.Instance.OnClick += InteractEvent;
+            _layerMask = LayerMask.NameToLayer("UI");
+        }
+
+        private void InteractEvent()
+        {
+            Debug.Log("Hello");
+            RaycastHit hit;
+            Ray ray = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
+            Debug.DrawRay(ray.origin, ray.direction * 5f, Color.green, 5f);
+            if (Physics.Raycast(ray, out hit, 5f, _layerMask))
+            {
+                if (hit.collider.GetComponent<Button>() != null)
+                {
+                    BaseEventData baseEventData = new BaseEventData(EventSystem.current);
+                    hit.collider.GetComponent<Button>().OnSubmit(baseEventData);
+                }
+            }
+        }
+    }
+}
